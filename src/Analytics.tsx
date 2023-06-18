3c9e4b31370215120da1fd70f93d509de630bcb2
Shrinandan Narayanan <emailshri.n@gmail.com>
2023-06-18 06:38:03 -0700

    s

--- src/Analytics.tsx
+++ src/Analytics.tsx
@@ -1,115 +1,120 @@
-import React, { useState } from 'react';
-import {
-  Box,
-  CssBaseline,
-  Select,
-  MenuItem,
-  FormControl,
-  InputLabel,
-  createTheme,
-  ThemeProvider,
-} from '@mui/material';
-import { styled } from '@mui/material/styles';
-import { SelectChangeEvent } from '@mui/material';
-
-const Container = styled('div')({
-  display: 'flex',
-  height: '100vh',
-});
-
-const Overlay = styled('div')({
-  position: 'relative',
-  flex: 1,
-});
-
-const LeftHalf = styled('div')({
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  width: '50%',
-  height: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-});
-
-const BlackBox = styled('div')({
-  flex: '60%',
-  padding: '16px',
-  position: 'relative',
-});
-
-const RedBox = styled('div')({
-  flex: '40%',
-  backgroundColor: 'red',
-});
-
-const ColorfulDiv = styled('div')<{ color: string }>(({ color }) => ({
-  position: 'absolute',
-  top: '80px',
-  left: '16px',
-  right: '16px',
-  bottom: '16px',
-  backgroundColor: color,
-}));
-
-const options = [
-  { value: 10, label: 'Ten', color: 'blue' },
-  { value: 20, label: 'Twenty', color: 'green' },
-  { value: 30, label: 'Thirty', color: 'orange' },
-];
-
-const theme = createTheme({
-  typography: {
-    fontWeightBold: 700,
-  },
-});
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { Chart } from "chart.js/auto";
 
 const AnalyticsView = () => {
-  const [age, setAge] = useState('');
+  const chartRef = useRef(null);
+  const [age, setAge] = useState("");
+  const [data, setData] = useState([]);
+  const [products, setProducts] = useState([] as any[]);
 
-  const handleChange = (event: SelectChangeEvent<string>) => {
-    setAge(event.target.value);
-  };
+  useEffect(() => {
+    fetchProductList();
+  }, []);
 
-  const selectedOption = options.find((option) => option.value === Number(age));
+  useEffect(() => {
+    if (age !== "") {
+      const selectedProduct = products.find((product) => product.name === age);
+      if (selectedProduct) {
+        const selectedId = selectedProduct.id;
+        fetchData(selectedId);
+      }
+    }
+  }, [age, products]);
+
+  async function fetchProductList() {
+    // Your API call function goes here
+    const endpoint = "http://localhost:8080/productlist";
+    axios
+      .get(endpoint)
+      .then((response) => {
+        setProducts(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  async function fetchData(id: any) {
+    const endpoint = "http://localhost:8080/clicks";
+    const requestBody = {
+      product_id: id,
+    };
+    axios
+      .post(endpoint, requestBody)
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+
+        // Clear the chart
+        if (chartRef.current) {
+          chartRef.current.destroy();
+        }
+
+        // Create a new chart
+        createChart(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  function createChart(data) {
+    const chartCanvas = chartRef.current.getContext("2d");
+    new Chart(chartCanvas, {
+      type: "line",
+      data: {
+        labels: data.map((point) => point.timestamp),
+        datasets: [
+          {
+            label: "PV",
+            data: data.map((point) => point.pv),
+            borderColor: "#8884d8",
+            fill: false,
+          },
+          {
+            label: "UV",
+            data: data.map((point) => point.uv),
+            borderColor: "#82ca9d",
+            fill: false,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+              text: "Date/Time",
+            },
+          },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: "Value",
+            },
+          },
+        },
+      },
+    });
+  }
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <CssBaseline />
-        <Overlay>
-          <LeftHalf>
-            <BlackBox>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Pro</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={age}
-                  label="Age"
-                  onChange={handleChange}
-                  sx={{
-                    minWidth: 120, // Adjust the width as needed
-                  }}
-                >
-                  {options.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-              {selectedOption && (
-                <ColorfulDiv color={selectedOption.color}>
-                  Colorful Div
-                </ColorfulDiv>
-              )}
-            </BlackBox>
-            <RedBox />
-          </LeftHalf>
-        </Overlay>
-      </Container>
-    </ThemeProvider>
+    <div>
+      <select value={age} onChange={(event) => setAge(event.target.value)}>
+        <option value="">Select a tab</option>
+        {products.map((product) => (
+          <option key={product.id} value={product.name}>
+            {product.name}
+          </option>
+        ))}
+      </select>
+      <canvas ref={chartRef} />
+    </div>
   );
 };
 
